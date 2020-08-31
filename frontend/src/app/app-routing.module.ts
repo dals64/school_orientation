059fea8d82e5dd6f3729168e18b1d4b9298e2d11
dals64 <lemotieuarold@gmail.com>
2020-08-31 15:46:13 +0200

    beginning of routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ListCareersComponent } from './list-careers/list-careers.component';
+import { AppBarComponent } from './app-bar/app-bar.component';
+import { ListSchoolsComponent } from './list-schools/list-schools.component';
+import { OutletsComponent } from './outlets/outlets.component';
+import { AppComponent } from './app.component';
+import { MentorComponent } from './mentor/mentor.component';
+import { BooksComponent } from './books/books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"/admin", component: SignInComponent, outlet:'principal'},
+  {path:"", component: AccueilComponent, outlet:'principal'},
+  {path:"/home", component: AppBarComponent, outlet:'principal', children:[
+    {path: "schools", component: ListSchoolsComponent, outlet:'application'},
+    {path:"", component: ListCareersComponent, outlet:'application'},
+    {path:"outlets", component:OutletsComponent, outlet:'application'},
+    {path:"mentors", component:MentorComponent, outlet:'application'},
+    {path:"books", component:BooksComponent, outlet:'application'}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/outlets/outlets.component.ts
+++ frontend/src/app/outlets/outlets.component.ts
@@ -7,6 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OutletsComponent implements OnInit {
 
+  list = [
+    {
+      name: 'outlet 1',
+      description: 'Consectetur non qui Lorem eu commodo cillum aliqua velit. Commodo sit tempor nulla quis exercitation anim. Labore ut sit ad sunt dolore aute irure est Lorem in commodo dolor culpa. Labore dolore quis aute eiusmod aliqua ut pariatur esse ad sit. Enim occaecat elit Lorem qui dolor et. Aute veniam magna culpa sunt veniam incididunt labore fugiat aliqua irure officia nulla consequat sint. Sunt aliqua cillum magna veniam tempor eiusmod nostrud commodo qui.'
+    },
+    {
+      name: 'outlet 2',
+      description: 'Consectetur non qui Lorem eu commodo cillum aliqua velit. Commodo sit tempor nulla quis exercitation anim. Labore ut sit ad sunt dolore aute irure est Lorem in commodo dolor culpa. Labore dolore quis aute eiusmod aliqua ut pariatur esse ad sit. Enim occaecat elit Lorem qui dolor et. Aute veniam magna culpa sunt veniam incididunt labore fugiat aliqua irure officia nulla consequat sint. Sunt aliqua cillum magna veniam tempor eiusmod nostrud commodo qui.'
+    },
+    {
+      name: 'outlet 3',
+      description: 'Consectetur non qui Lorem eu commodo cillum aliqua velit. Commodo sit tempor nulla quis exercitation anim. Labore ut sit ad sunt dolore aute irure est Lorem in commodo dolor culpa. Labore dolore quis aute eiusmod aliqua ut pariatur esse ad sit. Enim occaecat elit Lorem qui dolor et. Aute veniam magna culpa sunt veniam incididunt labore fugiat aliqua irure officia nulla consequat sint. Sunt aliqua cillum magna veniam tempor eiusmod nostrud commodo qui.'
+    },
+    {
+      name: 'outlet 4',
+      description: 'Consectetur non qui Lorem eu commodo cillum aliqua velit. Commodo sit tempor nulla quis exercitation anim. Labore ut sit ad sunt dolore aute irure est Lorem in commodo dolor culpa. Labore dolore quis aute eiusmod aliqua ut pariatur esse ad sit. Enim occaecat elit Lorem qui dolor et. Aute veniam magna culpa sunt veniam incididunt labore fugiat aliqua irure officia nulla consequat sint. Sunt aliqua cillum magna veniam tempor eiusmod nostrud commodo qui.'
+    },
+    {
+      name: 'outlet 5',
+      description: 'Consectetur non qui Lorem eu commodo cillum aliqua velit. Commodo sit tempor nulla quis exercitation anim. Labore ut sit ad sunt dolore aute irure est Lorem in commodo dolor culpa. Labore dolore quis aute eiusmod aliqua ut pariatur esse ad sit. Enim occaecat elit Lorem qui dolor et. Aute veniam magna culpa sunt veniam incididunt labore fugiat aliqua irure officia nulla consequat sint. Sunt aliqua cillum magna veniam tempor eiusmod nostrud commodo qui.'
+    },
+    {
+      name: 'outlet 6',
+      description: 'Consectetur non qui Lorem eu commodo cillum aliqua velit. Commodo sit tempor nulla quis exercitation anim. Labore ut sit ad sunt dolore aute irure est Lorem in commodo dolor culpa. Labore dolore quis aute eiusmod aliqua ut pariatur esse ad sit. Enim occaecat elit Lorem qui dolor et. Aute veniam magna culpa sunt veniam incididunt labore fugiat aliqua irure officia nulla consequat sint. Sunt aliqua cillum magna veniam tempor eiusmod nostrud commodo qui.'
+    },
+    {
+      name: 'outlet 7',
+      description: 'Consectetur non qui Lorem eu commodo cillum aliqua velit. Commodo sit tempor nulla quis exercitation anim. Labore ut sit ad sunt dolore aute irure est Lorem in commodo dolor culpa. Labore dolore quis aute eiusmod aliqua ut pariatur esse ad sit. Enim occaecat elit Lorem qui dolor et. Aute veniam magna culpa sunt veniam incididunt labore fugiat aliqua irure officia nulla consequat sint. Sunt aliqua cillum magna veniam tempor eiusmod nostrud commodo qui.'
+    },
+    {
+      name: 'outlet 9',
+      description: 'Consectetur non qui Lorem eu commodo cillum aliqua velit. Commodo sit tempor nulla quis exercitation anim. Labore ut sit ad sunt dolore aute irure est Lorem in commodo dolor culpa. Labore dolore quis aute eiusmod aliqua ut pariatur esse ad sit. Enim occaecat elit Lorem qui dolor et. Aute veniam magna culpa sunt veniam incididunt labore fugiat aliqua irure officia nulla consequat sint. Sunt aliqua cillum magna veniam tempor eiusmod nostrud commodo qui.'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
